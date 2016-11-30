dd0e0c3046e77307d8cf18a3b631c67b58c245ba
John <johnreyroco@vista.aero>
2016-11-30 03:54:40 +0000

    add databaseurl environment

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ var commentRoutes = require("./routes/comments"),
 //seedDB();
 mongoose.Promise = global.Promise;
 // mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect("mongodb://jrr:jrr@ds115798.mlab.com:15798/jrrdb")
+// mongoose.connect("mongodb://jrr:jrr@ds115798.mlab.com:15798/jrrdb");
+mongoose.connect(process.env.DATABASEURL);
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
